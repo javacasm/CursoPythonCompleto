6c67000ad7837dcf99e71ea2da942750e9280547
makova <makova65@gmail.com>
2016-06-22 23:18:22 +0200

    otros dos ejercicios

---
+++ Ejemplos_TKinter/otra_ventana_etiqueta.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  otra_ventana_etiqueta.py
+#    
+
+
+from Tkinter import *
+
+Ventana = Tk()
+
+Etiqueta = Label(Ventana,text="Esto es una etiqueta.")
+
+Etiqueta.pack()
+
+Ventana.mainloop()
---
+++ Ejemplos_TKinter/una_ventana.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  una_ventana.py
+#  
+
+
+from Tkinter import *  # Importamos todo con el asterisco
+
+ventana = Tk()         # Declaramos una variable y añadimos la función Tk()
+
+ventana.mainloop()     # A traves de ventana llamamos a la función maninloop() 
+                       # que es un bucle principal y hace que se mantenga 
+                       # abierta la ventana.
+
+

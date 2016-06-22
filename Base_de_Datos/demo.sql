28f17712cc7d83110801f057274226d3e625ca2b
toblerone554 <toblerone554@gmail.com>
2016-06-22 19:38:20 +0200

    Modificando Base Datos

--- Base_de_Datos/demo.sql
+++ Base_de_Datos/demo.sql
@@ -4,4 +4,12 @@ use demo;
 
 create table productos (nombre VARCHAR (20));
 
+describe productos;
+
 insert into productos (nombre) values ('Impresora');
+
+insert into productos (nombre) values ('Portatil');
+
+insert into productos (nombre) values ('Pantalla');
+
+select * from productos
---
+++ Base_de_Datos/hola.py
@@ -0,0 +1,3 @@
+dias = {'lunes': 1, 'martes': 2,'miercoles': 3}
+for clave, valor in dias.iteritems():
+	print "El valor de la clave %s es %s" % (clave, valor)

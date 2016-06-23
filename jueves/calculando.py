2f915c7700d16f49896d270eb9f67828d0a2299b
makova <makova65@gmail.com>
2016-06-24 00:56:09 +0200

    algunos ejercicios del jueves

---
+++ jueves/calculando.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  calculando.py
+#  
+
+
+print("***Calculo del área de un triangulo***")
+base=6;altura=2; base,altura=altura,base
+calculo=(base*altura)/2
+print("La base es:", base, "La altura es:", altura)
+print("El resultado es :", calculo)
---
+++ jueves/importando.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  importando.py
+#
+
+
+import mas_metodos            
+
+print "esto es un programa que importa"
+
+mas_metodos.adios()
+
+print mas_metodos.mi_cadena
